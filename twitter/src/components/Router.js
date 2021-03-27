d444c23946f10da9fb902b9c31f62bdb2dd82c28
qhahd78 <umuj0708@likelion.org>
2021-03-28 00:52:38 +0900

    Logout

--- twitter/src/components/Router.js
+++ twitter/src/components/Router.js
@@ -1,24 +1,37 @@
 import React, {useState} from 'react';
-import { HashRouter as Router, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
 
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Navigation from 'components/Navigation';
+import Profile from 'routes/Profile';
 
 // 파라미터로 props 전달 
 function AppRouter({ isLoggedIn }) {
     return (
        <Router>
+           {/* isLoggedIn이 True 면 Navigation 이 존재.  */}
+           {isLoggedIn && <Navigation />}
            <Switch>
-            {isLoggedIn ? 
+            {isLoggedIn ? (
             <>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            </> : 
-            <Route exact path="/">
-                <Auth />   
-            </Route>}
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/profile">
+                    <Profile />
+                </Route>
+                <Redirect from="*" to="/" />
+            </> 
+            ): ( 
+            <>
+                <Route exact path="/">
+                    <Auth />   
+                </Route>
+                <Redirect from="*" to="/" />
+            </>
+            )}
            </Switch>
        </Router>
     )
--- twitter/src/routes/Profile.js
+++ twitter/src/routes/Profile.js
@@ -1,11 +1,18 @@
+import { authService } from 'firebasekeys';
 import React from 'react'
+import { useHistory } from 'react-router';
 
 function Profile() {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    }
     return (
-        <div>
-            <span>Profile</span>
-        </div>
-    )
-}
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
 
 export default Profile

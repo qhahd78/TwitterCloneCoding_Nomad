5c2188b02e97a195aca3514751b80f7d2b9ec4ac
qhahd78 <umuj0708@likelion.org>
2021-03-29 19:36:06 +0900

    Delete and Update part One

--- twitter/src/routes/Home.js
+++ twitter/src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { dbService } from "firebasekeys";
+import Nweet from 'components/Nweet';
 
 function Home({ userObj }) {
     // console.log(userObj.email);
@@ -59,9 +60,11 @@ function Home({ userObj }) {
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                   <Nweet 
+                    key={nweet.id} 
+                    nweetObj={nweet} 
+                    isOwner={nweet.creatorId === userObj.uid} 
+                    />
                 ))}
             </div>
         </div>
